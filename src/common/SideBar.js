e86a28b4551eaa93362841a28890e62c80761510
Damian Phung <damianphung@gmail.com>
2020-06-11 17:16:09 +1000

    clean up

--- src/common/SideBar.js
+++ src/common/SideBar.js
@@ -8,27 +8,16 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { Typography } from '@material-ui/core';
-import { useState } from 'react';
-import { useMediaQuery } from '@material-ui/core';
-import { useTheme } from '@material-ui/core/styles';
-import { Badge, Hidden } from '@material-ui/core';
-
-import MenuIcon from '@material-ui/icons/Menu';                                                                                                                                               
-import InputIcon from '@material-ui/icons/Input';  
 
 import Link from './Link';
 import { makeStyles } from '@material-ui/core/styles';
 import { createFragmentContainer, graphql } from 'react-relay';
-import { useConfig, useHistory, useAuth } from '../hooks';
 
 import Drawer from '@material-ui/core/Drawer';
 import {
   Divider,
   List,
   ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
 } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
@@ -82,29 +71,9 @@ function SideBar(props) {
       onClose,
       ...other
     } = props;
-    const [userMenuEl, setUserMenuEl] = React.useState(null);
-    const { app } = useConfig();
-    const history = useHistory();
-    const auth = useAuth();
-    const s = useStyles();
-    
 
-    function handleClose() {
-      history.replace('/');
-    }
-  
-    function openUserMenu(event) {
-      setUserMenuEl(event.currentTarget);
-    }
-  
-    function closeUserMenu() {
-      setUserMenuEl(null);
-    }
-  
-    function signIn() {
-      closeUserMenu();
-      auth.signIn();
-    }
+    const s = useStyles();
+
   //          className={clsx(s.root, className)} 
     return (
         <Drawer 
@@ -114,51 +83,47 @@ function SideBar(props) {
           open={open}
           onClose={onClose}
           variant={variant} >        
-                {me && (
-                  <div className={clsx(s.avatarRoot, className)}>
-                    <IconButton
-                      className={s.avatarButton}
-                      onClick={openUserMenu}
-                      aria-owns={userMenuEl ? 'user-menu' : null}
-                      aria-haspopup="true"
-                    >
-                      <Avatar
-                        className={s.avatar}
-                        src={me.photoURL}
-                        alt={me.displayName}
-                      />
-                      
-                    </IconButton>
-                    <Typography>
-                      Welcome
-                    </Typography>
-                    <Typography>
-                    {me.displayName}
-                    </Typography>
-                    <Divider className={s.divider} />
-                  </div> 
-                )}
-                
-                <List className={clsx(s.root, className)} disablePadding >
-                  <ListItem  className={clsx(s.listItem, className)} >                   
-                  {!me && (
-                    <Button className={s.button} color="inherit" onClick={signIn}>
-                      Log In / Sign Up
-                    </Button>
-                  )}                
-                  </ListItem> 
-                  <ListItem  className={clsx(s.listItem, className)} >        
-                    <Button color="inherit" component={Link} href="/news">
-                      News
-                    </Button>
-                  </ListItem> 
-                  <ListItem  className={clsx(s.listItem, className)} > 
-
-                  <Button color="inherit" component={Link} href="/about">
-                    About
-                  </Button> 
-                  </ListItem>                  
-            </List>
+          {me && (
+            <div className={clsx(s.avatarRoot, className)}>
+              <IconButton
+                className={s.avatarButton}
+                aria-haspopup="true"
+              >
+                <Avatar
+                  className={s.avatar}
+                  src={me.photoURL}
+                  alt={me.displayName}
+                />
+              </IconButton>
+              <Typography>
+                Welcome
+              </Typography>
+              <Typography>
+              {me.displayName}
+              </Typography>
+              <Divider className={s.divider} />
+            </div> 
+          )}
+          
+          <List className={clsx(s.root, className)} disablePadding >
+            {/* <ListItem  className={clsx(s.listItem, className)} >                   
+            {!me && (
+              <Button className={s.button} color="inherit" onClick={signIn}>
+                Log In / Sign Up
+              </Button>
+            )}                
+            </ListItem>  */}
+            <ListItem  className={clsx(s.listItem, className)} >        
+              <Button color="inherit" component={Link} href="/news">
+                News
+              </Button>
+            </ListItem> 
+            <ListItem  className={clsx(s.listItem, className)} > 
+              <Button color="inherit" component={Link} href="/about">
+                About
+              </Button> 
+            </ListItem>                  
+          </List>
         </Drawer>
     );
 }
